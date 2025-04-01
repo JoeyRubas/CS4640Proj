88e8b6534e49235536b00be5c563be0d943b03b2
Vincent Martinez <vincentcruzmartinez@gmail.com>
2025-03-31 20:51:12 -0400

    added autoloading of db

--- index.php
+++ index.php
@@ -4,31 +4,49 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-// Class autoloading by name.  All our classes will be in a directory
-// that Apache does not serve publicly.  They will be in C:/xampp/htdocs/, which
-// is our src/ directory in Docker.
+// Class autoloading by name
 spl_autoload_register(function ($classname) {
-        $path = "CS4640chess/$classname.php";
-        if (file_exists($path)) {
-            include $path;
-        }
+    $path = "CS4640chess/$classname.php";
+    if (file_exists($path)) {
+        include $path;
+    }
 });
 
-// CS4640 server
-// public files: public_html
-// private files: anything OUTSIDE of public_html
-//     create a "private" next to public_html
-//     include "/students/mst3k/students/mst3k/private"
-//
-//     if I created "private/anagramsgame"
-//     include "/students/mst3k/students/mst3k/private/anagramsgame/$classname.php";:
+function checkDatabaseSetup() {
+    $host = "db";
+    $port = "5432";
+    $database = "example";
+    $user = "localuser";
+    $password = "cs4640LocalUser!";
 
+    $dbHandle = pg_connect("host=$host port=$port dbname=$database user=$user password=$password");
 
-// Other global things that we need to do
+    if (!$dbHandle) {
+        return false;
+    }
 
-// Instantiate the front controller
-$anagrams = new ChessController($_GET);
+    // Check if our tables exist
+    $res = pg_query($dbHandle, "SELECT EXISTS (
+        SELECT FROM information_schema.tables 
+        WHERE table_schema = 'public' 
+        AND table_name = 'chess_users'
+    );");
 
-// Run the controller
-$anagrams->run();
+    $tablesExist = pg_fetch_result($res, 0, 0) === 't';
+    pg_close($dbHandle);
 
+    return $tablesExist;
+}
+
+function initializeDatabase() {
+    include 'databaseinit.php';
+}
+
+// Check if database is set up, if not, initialize it
+if (!checkDatabaseSetup()) {
+    initializeDatabase();
+}
+
+$chessController = new ChessController($_GET);
+
+$chessController->run();
